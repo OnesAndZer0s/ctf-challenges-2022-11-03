1e61e66571bba42d7351a9522a5782428081f6c6
Michael Zhang <mail@mzhang.io>
2022-11-03 17:36:59 -0500

    Revert "util"
    This reverts commit 933f46ac5519066f23291c7ebeec8e9aa6257c83.

--- servers/caesar-break.py
+++ servers/caesar-break.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
 import random
+import string
 
-from .util import caesar_shift, randstring
+def caesar_shift(s, n):
+    alpha = string.ascii_lowercase + string.ascii_uppercase
+    translated = string.ascii_lowercase[n:] + string.ascii_lowercase[:n] + string.ascii_uppercase[n:] + string.ascii_uppercase[:n]
+    tab = str.maketrans(alpha, translated)
+
+    s2 = ""
+    for c in s:
+        if c in string.ascii_letters:
+            s2 += c.translate(tab)
+        else:
+            s2 += c
+    return s2
 
 for i in range(1024):
     n = random.randint(1, 25)
-    s = randstring(25)
+    s = "".join(random.choices(string.ascii_letters, k=25))
     print(caesar_shift(f"This string is rotated by an unknown amount: {s}", n))
 
     a = input("Original string: ").strip()
--- servers/xor.py
+++ servers/xor.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
 import random
+import string
 from binascii import hexlify, unhexlify
 
-from .util import randstring, xor
+def xor(p, k):
+    s = []
+    for i, c in enumerate(p):
+        kc = k[i % len(k)]
+        s.append(ord(c) ^ ord(kc))
+    return bytearray(s)
 
 for i in range(1024):
     n = random.randint(5, 10)
-    k = randstring(n)
+    k = "".join(random.choices(string.ascii_letters, k=n))
     print(f"The key is {n} letters long.")
 
-    s = randstring(25)
+    s = "".join(random.choices(string.ascii_letters, k=25))
     sh = hexlify(s.encode("utf-8"))
     print(f"Hex(Original string): {sh}")
-
     e = hexlify(xor(s, k))
     print(f"Hex(Xor(Encrypted, Key)): {e}")
 
